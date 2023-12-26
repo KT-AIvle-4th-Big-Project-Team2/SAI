13fad7e3c63cedc2d997d978c6f63cf612ba8b00
LVNAo <141792401+LVNAo@users.noreply.github.com>
2023-12-26 17:11:54 +0900

    [231226]마무리_KS

--- front-end/src/screens/Simul/Simul.js
+++ front-end/src/screens/Simul/Simul.js
@@ -1,13 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Box } from '@mui/material'
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
 
 export default function SelectVariants() {
-  const [age, setAge] = React.useState('');
+  const [Gu, setGu] = useState('')
 
   const Gu_SELECT = ['종로구',
     '중구',
@@ -37,49 +33,19 @@ export default function SelectVariants() {
   const Dong_SELECT = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setGu(event.target.value);
   };
 
   return (
     <>
     <Box sx = {{mt : 3, mb : 5 }}><h2>지역 선택</h2></Box>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={Gu_SELECT}
-          onChange={handleChange}
-          label="Age"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
           
-          {
-            Gu_SELECT.map((year, idx) => {
-              return <MenuItem key={idx} value={year}>{year}</MenuItem>
-            })
-          }
+      {Gu_SELECT.map((Gu, idx) => {
+        return <Button>{Gu}</Button>
+        
+      })}
           
-        </Select>
-      </FormControl>
-      <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-filled-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
+          
       <Button href = '/SimulReport' size='large'>리포트 보기</Button>
     </>
   );
