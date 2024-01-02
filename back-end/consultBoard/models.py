e073faed3e822e68acb75aec7d51ecb39dda4f09
morning772 <morning772@naver.com>
2024-01-02 11:54:38 +0900

    [240102]게시판models오타수정_WH

--- back-end/consultBoard/models.py
+++ back-end/consultBoard/models.py
@@ -27,7 +27,7 @@ class BoardConsult(models.Model):
     title = models.CharField(max_length=75)
     contents = models.TextField()
     tag = models.CharField(max_length=75)
-    creationdate = models.DateTimeField()
+    creationdate = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, models.DO_NOTHING)
 
     class Meta:
@@ -38,7 +38,7 @@ class BoardConsult(models.Model):
 class CommentsConsult(models.Model):
     comment_id = models.IntegerField(primary_key=True)
     contents = models.TextField()
-    creationdate = models.DateTimeField()
+    creationdate = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, models.DO_NOTHING)
     board = models.ForeignKey(BoardConsult, models.DO_NOTHING)
 
--- back-end/dev/urls.py
+++ back-end/dev/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
     path('board/', include('board.urls')),
+    path('concernboard/', include('concernBoard.urls')),
+    path('consultboard/', include('consultBoard.urls')),
+    path('reviewboard/', include('reviewBoard.urls')),
     path('announcements/', include('announcement.urls')),
     path('faq/', include("faq.urls"))
 ]
